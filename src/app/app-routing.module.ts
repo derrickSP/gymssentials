729ae6bff22ec08cd04ba4040c422f15cde1e8e8
derrickSP <derricklimkaihao7.17@ichat.sp.edu.sg>
2019-11-19 18:03:25 +0800

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { HomePageComponent } from "./home-page/home-page.component";
-const routes: Routes = [{ path: "home", component: HomePageComponent }];
+const routes: Routes = [
+  { path: "home", component: HomePageComponent },
+  { path: "**", redirectTo: "/home" },
+  { path: "", redirectTo: "/home", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
