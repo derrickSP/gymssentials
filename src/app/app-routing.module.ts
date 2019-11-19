2a0b982cc1e1c47ccd7df156dc141a51bf0a113f
derrickSP <derricklimkaihao7.17@ichat.sp.edu.sg>
2019-11-19 17:41:55 +0800

    UPDATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+import { HomePageComponent } from "./home-page/home-page.component";
+const routes: Routes = [{ path: "home", component: HomePageComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
