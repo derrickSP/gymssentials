c8922b1240ffcddd53e753db033bc53e55d13fd0
derrickSP <derricklimkaihao7.17@ichat.sp.edu.sg>
2019-11-20 09:55:26 +0800

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { ShopComponent } from './shop/shop.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { ShopComponent } from "./shop/shop.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
 
 @NgModule({
   declarations: [
@@ -14,11 +14,8 @@ import { ProductDetailComponent } from './product-detail/product-detail.componen
     ShopComponent,
     ProductDetailComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-shop",
@@ -6,7 +7,18 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./shop.component.scss", "../app.component.scss"]
 })
 export class ShopComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+  gotoProductDetails(url, id) {
+    var myurl = `${url}/${id}`;
+    window.location.href = myurl;
+    // this.router.navigateByUrl(myurl).then(e => {
+    //   if (e) {
+    //     console.log("Navigation is successful!");
+    //   } else {
+    //     console.log("Navigation has failed!");
+    //   }
+    // });
+  }
 }
